8bfa60df5156f678058b1e848ac1a345b7320672
v4724 <v47244724@gmail.com>
2025-07-14 16:17:44 +0800

    chore: 發佈資訊

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    return {
+	return {
+		base: `/nice-0816/`,
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
