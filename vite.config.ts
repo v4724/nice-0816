c1fd54d8a502acbe6949c4c53475f86db63554e3
v4724 <v47244724@gmail.com>
2025-08-07 00:55:00 +0800

    core: 部署設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: `/nice-0816/`,
+    base: process.env.VITE_BASE_PATH || '/',
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
